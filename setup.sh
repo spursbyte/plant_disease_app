ee6a2c1bb51459998a6de24bdf6912e3cb0acb93
Siddharth Mohanty <60615807+spursbyte@users.noreply.github.com>
2021-04-25 22:14:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"shubham.aiengineer@gmail.com\"\n\
+email = \"siddharthmohanty1234@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
