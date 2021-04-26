5e93136a709935465d2e4f8be9f8371ed16f0dad
Siddharth Mohanty <60615807+spursbyte@users.noreply.github.com>
2021-04-26 23:27:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ category_index = label_map_util.create_category_index(categories)
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
+    od_graph_def = tf.compat.v1.GraphDef()
     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
